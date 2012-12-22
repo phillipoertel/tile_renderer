d70b9f33548913db38cd2130b1773eaaa12ac558
Phillip Oertel <me@phillipoertel.com>
2012-12-22 17:01:41 +0100

    Simplified spec, also using rspec-given

--- spec/tile_renderer_spec.rb
+++ spec/tile_renderer_spec.rb
@@ -1,23 +1,14 @@
 require 'spec_helper'
 
-def tile_for(content)
-  TileRenderer.render(content)
-end
-
 describe "TileRenderer with renderered view (to test integration)" do
   
   context "A link with teaser image" do
-    Given(:html) { tile_for(Link.new(link: 'http://www.sinatrarb.com', teaser: "TEASER IMAGE")) }
-    it "has content.teaser as image" do
-      html.should have_tag("div[class=image]", /TEASER IMAGE/)
-    end
-    it "has a link in the footer" do
-      html.should have_tag("div[class=footer]", %r{http://www.sinatrarb.com})
-    end
-    it "has additional css classes" do
-      html.should have_tag("div[class~=show-image]")
-      html.should have_tag("div[class~=show-footer]")
-    end
+    Given(:content) { Link.new(link: 'http://www.sinatrarb.com', teaser: "TEASER IMAGE") }
+    When(:html) { TileRenderer.render(content) }
+    Then { html.should have_tag("div[class=image]", /TEASER IMAGE/) }
+    And { html.should have_tag("div[class=footer]", %r{http://www.sinatrarb.com}) }
+    And { html.should have_tag("div[class~=show-image]") }
+    And { html.should have_tag("div[class~=show-footer]") }
   end
   
 end
