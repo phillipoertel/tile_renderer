db0253648dff5c917601c703d527f4e1b5825d23
Phillip Oertel <me@phillipoertel.com>
2012-12-22 17:40:47 +0100

    Simplified image handling, removing almost duplicate modules

---
+++ lib/rendering/template_data/with_image.rb
@@ -0,0 +1,24 @@
+module WithImage
+
+  # show the image
+  def image
+    if (content.has_preview? && content.has_teaser?)
+      content.teaser
+    elsif content.has_preview?
+      content.preview
+    elsif content.has_teaser?
+      content.teaser
+    end
+  end
+  
+  # shorten text
+  def text
+    super && super[0...25]
+  end
+  
+  # set css so that header div is shown
+  def css_classes
+    super + ['show-image']
+  end
+  
+end
