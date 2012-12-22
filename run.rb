d984df14c6aa0aa5ff81c87e858497b866b1f905
Phillip Oertel <me@phillipoertel.com>
2012-12-22 16:39:35 +0100

    Fixed run.rb

--- run.rb
+++ run.rb
@@ -1,8 +1,11 @@
 $LOAD_PATH.unshift('.')
 require 'lib/tile_renderer'
 
-video = Video.new(preview: 'video.mpg', teaser: 'teaser.mp4')
-p TileRenderer.for(video).render
+video = Video.new(preview: 'video.mpg')
+puts TileRenderer.render(video)
+
+text = Text.new(teaser: 'teaser.mp4', description: "bla bla " * 100)
+puts TileRenderer.render(text)
 
 link = Link.new(link: 'http://www.sinatrarb.com')
-p TileRenderer.for(link).render
+puts TileRenderer.render(link)
