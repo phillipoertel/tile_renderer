fb39a51e15d88fc74c93ad83bb9c1143e44ee81f
Phillip Oertel <me@phillipoertel.com>
2012-12-22 15:34:38 +0100

    Don't always extend the renderer with a class-specific module.

--- lib/rendering/tile_renderer.rb
+++ lib/rendering/tile_renderer.rb
@@ -19,16 +19,14 @@ class TileRenderer
   private
   
     # sets self up to render the given content correctly
-    # assign to rendering_extensions first so this can be debugged easily (see attr_reader)
+    # assign to @extensions first so this can be debugged easily (see attr_reader)
     def configure_renderer!
-      extensions = [DefaultRenderer]
-      extensions << content_rendering_adapter
-      extensions << WithPreview if content.has_preview?
-      extensions << WithTeaser if content.has_teaser?
-      extensions.each { |e| extend(e) }
+      @extensions = [DefaultRenderer]
+      @extensions << Adapter::Link if content.is_a?(Link)
+      @extensions << Adapter::HtmlText if content.is_a?(HtmlText)
+      @extensions << WithPreview if content.has_preview?
+      @extensions << WithTeaser if content.has_teaser?
+      @extensions.each { |e| extend(e) }
     end
-  
-    def content_rendering_adapter
-      Adapter.const_get(content.class.to_s.to_sym)
-    end
+
 end
